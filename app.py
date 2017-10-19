b89e0e98f68f8d0e5b8e14deda58c136a97dd620
Jon Byrum <jbyrum@jbyrum-ltm.internal.salesforce.com>
2017-10-19 14:55:34 -0700

    one request

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 import os
 import logging
 import risk
+import json
 from datetime import datetime, timedelta
 
-
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def home():
-    r = int(request.args.get("risk"))
-    health = risk.calculateRiskScore(r)
-    return jsonify(health)
+	data = request.get_json(force=True)
+	returnData = []
+	
+	for x in data:
+		returnData.append(risk.calculateRiskScore(x))
+	
+	return json.dumps(returnData)
 
